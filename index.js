ebd3202627aac1d0bea48b27d8f2b14e1514bdb9
idangvili <P0011111@ness-tech.co.il>
2017-07-13 19:31:34 +0300

    changes for azire

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    var member = require('./member.json'); 
+    var member = require('./member.json')
+    res.setHeader('Content-Type', 'application/json')
     res.send(member)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var port = process.env.PORT || 1337;
+app.listen(port, function () {
+  console.log('Example app listening on port %d!',port)
 })
 
 

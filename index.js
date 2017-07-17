c1cb52151be233756405f0fe0e6e6b0d01a29e96
idangvili <P0011111@ness-tech.co.il>
2017-07-17 19:18:35 +0300

    change logic

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    var member = require('./member.json')
+app.get('/members/:memberid', function (req, res) {
+    var memberid = req.params.memberid;
     res.setHeader('Content-Type', 'application/json')
+    //res.send(memberid);
+    //res.send('./data/'+ memberid + '.json');
+    var member = require('./data/'+ memberid + '.json')
     res.send(member)
 })
 

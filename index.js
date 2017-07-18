2b1b8f87ed6bd071c7a19147462b029ee1d2d01c
idangvili <P0011111@ness-tech.co.il>
2017-07-18 15:19:27 +0300

    change data files

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const app = express()
+const app = express()  
 
-app.get('/members/:memberid', function (req, res) {
+
+app.get('/members/:memberid', 
+  function (req, res, next ) {
+
     var memberid = req.params.memberid;
     //res.send(memberid);
     //res.send('./data/'+ memberid + '.json');
     var member = require('./data/'+ memberid + '.json')
-    res.setHeader('Content-Type', 'application/json')
-    res.send(member)
-})
+    //res.setHeader('Content-Type', 'application/json')
+    //res.send(member)
+    res.status(200).json(member);
+    res.status(403).send('ness-pension-digital no such data')
+    res.status(404).send('ness-pension-digital no such data')
+    res.status(500).send('ness-pension-digital no such data')
+   
+  }
+)
 
 var port = process.env.PORT || 1337;
 app.listen(port, function () {
